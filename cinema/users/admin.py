d00577c93f31192559158b2a923fb2a944b3f15d
Pavel Ivanov <pavel.ivanov.060988@gmail.com>
2023-05-23 20:49:09 +0300

    Register ClubCard and Feedback models in admin-panel

--- cinema/users/admin.py
+++ cinema/users/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import User
+from .models import User, ClubCard, Feedback
 
 
 admin.site.register(User)
+admin.site.register(ClubCard)
+admin.site.register(Feedback)
